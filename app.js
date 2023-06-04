8324cebeb7c64a9b6a4a45481efc901bcfb37e03
BaNg-W <bang344251871@gmail.com>
2023-06-04 11:35:55 -0400

    aasd

--- app.js
+++ app.js
@@ -63,3 +63,15 @@ playButton.onclick = function() {
     audioElement.src = url;
     audioElement.play();
 };
+
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
